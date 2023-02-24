7257db1ee761466b492aa650d8f2490ccd1a2451
shuluke1030 <shuluke1030@gmail.com>
2023-02-24 17:43:12 +0800

    finish

---
+++ main.js
@@ -0,0 +1,9 @@
+function check(a, x) {
+  for(let i = 0; i < a.length; i++){
+    if( a[i] === x){
+        return true;
+    }
+  }
+  return false;
+}
+check(['what', 'a', 'great', 'kata'], 'kat');
